e265a66175ceff300ede1dacdfbe3a80d6f9feaa
Afsal <afsal@ionaught.com>
2023-03-13 19:17:13 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import React, { lazy } from 'react'
+import { Suspense } from 'react'
+import env from 'env'
+
+function App() {
+  return (
+      <h1>Container</h1>
+  )
+}
+
+export default App
